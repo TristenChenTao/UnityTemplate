d595682f23d2613d7c4775b06c46ab0fc1768782
Tristen陈涛 <TristenChenTao@gmail.com>
2017-11-10 16:36:29 +0800

    初始化
    加入 ShareSDK 4.0.4
    加入 VSCode 插件

---
+++ Assets/Scripts/MainScene.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using cn.sharesdk.unity3d;
+using UnityEngine.UI;  
+
+public class MainScene : MonoBehaviour {
+
+	private ShareSDK ssdk;
+	// Use this for initialization
+	void Start () {
+		ssdk = gameObject.GetComponent<ShareSDK>();
+        ssdk.authHandler = AuthResultHandler; 
+	}
+
+    public void Login(){
+            ssdk.Authorize(PlatformType.WeChat);
+    }
+
+	void AuthResultHandler(int reqID, ResponseState state, PlatformType type, Hashtable result)
+    {
+        if (state == ResponseState.Success)
+        {
+            print("authorize success !");
+
+            if(type == PlatformType.WeChat) {
+
+            }
+            else if (type == PlatformType.QQ)
+            {
+
+            }
+            else if (type == PlatformType.SinaWeibo)
+            {
+
+            }            
+
+        }
+        else if (state == ResponseState.Fail)
+        {
+            print("fail! error code = " + result["error_code"] + "; error msg = " + result["error_msg"]);
+        }
+        else if (state == ResponseState.Cancel)
+        {
+            print("cancel !");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
